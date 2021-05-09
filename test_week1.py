b4b9f627d2f12578a2c08e3d0ea639de7f61b53f
Samperebikovsky <samperebikovsky@ucsb.edu>
2021-05-08 19:50:43 -0700

    Adding tests

--- test_week1.py
+++ test_week1.py
@@ -39,24 +39,38 @@ class TestVaccineFunction(unittest.TestCase):
                     clear_tables(sqlClient)
                     self.fail("Vaccine initialization failed")
     
-    # def testInvalid_vaccine(self):
-    #     with SqlConnectionManager(Server=os.getenv("Server"),
-    #                                   DBname=os.getenv("DBName"),
-    #                                   UserId=os.getenv("UserID"),
-    #                                   Password=os.getenv("Password")) as sqlClient:
-    #         with sqlClient.cursor(as_dict=True) as cursor:
-    #             try:
-    #                 # clear the tables before testing
-    #                 clear_tables(sqlClient)
-    #                 # create a new VaccineCaregiver object
-    #                 self.vaccine_a = covid(vaccine="MicroChipInsideYou",
-    #                                                 cursor=cursor)
-    #                 # check if the patient is correctly inserted into the database
-    #                 sqlQuery = '''
-    #                             SELECT VaccineName
-    #                             FROM Vaccine
-    #                             WHERE VaccineName = 'Pfizer';
-    #                             '''
+    def testInvalid_vaccine(self):
+        with SqlConnectionManager(Server=os.getenv("Server"),
+                                      DBname=os.getenv("DBName"),
+                                      UserId=os.getenv("UserID"),
+                                      Password=os.getenv("Password")) as sqlClient:
+            with sqlClient.cursor(as_dict=True) as cursor:
+                try:
+                    # clear the tables before testing
+                    clear_tables(sqlClient)
+                    # create a new VaccineCaregiver object
+                    self.vaccine_a = covid(vaccine="MicroChipInsideYou",
+                                                    cursor=cursor)
+                    # check if the patient is correctly inserted into the database
+                    sqlQuery = '''
+                                SELECT *
+                                FROM Vaccine;
+                               '''
+
+                    #self.vaccine_a.AddDoses(4, cursor)
+
+                    cursor.execute(sqlQuery)
+                    rows = cursor.fetchall()
+                    
+                    if rows[0]['VaccineName'] != '':
+                        self.fail("Vaccine naming failed")
+                    
+                    clear_tables(sqlClient)
+                except Exception:
+                    # In this case, the exception should be thrown to pass the test so no failure here.
+                    # clear the tables if an exception occurred
+                    clear_tables(sqlClient)
+                    
     
     def testAdd_dose(self):
 
@@ -128,8 +142,153 @@ class TestVaccineFunction(unittest.TestCase):
                     # clear the tables if an exception occurred
                     clear_tables(sqlClient)
                     self.fail("Reserving doses failed")
+    
+    
+    def testTemp_doseAZ(self):
+
+        with SqlConnectionManager(Server=os.getenv("Server"),
+                                  DBname=os.getenv("DBName"),
+                                  UserId=os.getenv("UserID"),
+                                  Password=os.getenv("Password")) as sqlClient:
+            with sqlClient.cursor(as_dict=True) as cursor:
+                try:
+                    # clear the tables before testing
+                    clear_tables(sqlClient)
+                    # create a new VaccineCaregiver object
+                    self.vaccine_a = covid(vaccine="AstraZeneca",
+                                                    cursor=cursor)
+                    # check if the patient is correctly inserted into the database
+                    sqlQuery = '''
+                                SELECT MaxStorageTemperature
+                                FROM Vaccine
+                                WHERE VaccineName = 'AstraZeneca'
+                                AND MaxStorageTemperature > 36
+                                '''
+
+                    self.vaccine_a.AddDoses(1, cursor)
+
+                    cursor.execute(sqlQuery)
+                    rows = cursor.fetchall()
+
+                    if rows != []:
+                        self.fail("Astrazeneca Temperature problem")
+
+                    clear_tables(sqlClient)
+
+                except Exception:
+                    # clear the tables if an exception occurred
+                    clear_tables(sqlClient)
+                    self.fail("Astrazeneca Temperature problem")
+
+
+    def testTemp_doseMD(self):
+
+        with SqlConnectionManager(Server=os.getenv("Server"),
+                                  DBname=os.getenv("DBName"),
+                                  UserId=os.getenv("UserID"),
+                                  Password=os.getenv("Password")) as sqlClient:
+            with sqlClient.cursor(as_dict=True) as cursor:
+                try:
+                    # clear the tables before testing
+                    clear_tables(sqlClient)
+                    # create a new VaccineCaregiver object
+                    self.vaccine_a = covid(vaccine="Moderna",
+                                                    cursor=cursor)
+                    # check if the patient is correctly inserted into the database
+                    sqlQuery = '''
+                                SELECT MaxStorageTemperature
+                                FROM Vaccine
+                                WHERE VaccineName = 'Moderna'
+                                AND MaxStorageTemperature > -20
+                                '''
+
+                    self.vaccine_a.AddDoses(1, cursor)
+
+                    cursor.execute(sqlQuery)
+                    rows = cursor.fetchall()
+
+                    if rows != []:
+                        self.fail("Moderna Temperature problem")
+
+                    clear_tables(sqlClient)
+
+                except Exception:
+                    # clear the tables if an exception occurred
+                    clear_tables(sqlClient)
+                    self.fail("Moderna Temperature problem")
+
+
+    def testTemp_dosePZ(self):
+
+        with SqlConnectionManager(Server=os.getenv("Server"),
+                                  DBname=os.getenv("DBName"),
+                                  UserId=os.getenv("UserID"),
+                                  Password=os.getenv("Password")) as sqlClient:
+            with sqlClient.cursor(as_dict=True) as cursor:
+                try:
+                    # clear the tables before testing
+                    clear_tables(sqlClient)
+                    # create a new VaccineCaregiver object
+                    self.vaccine_a = covid(vaccine="Pfizer",
+                                                    cursor=cursor)
+                    # check if the patient is correctly inserted into the database
+                    sqlQuery = '''
+                                SELECT MaxStorageTemperature
+                                FROM Vaccine
+                                WHERE VaccineName = 'Pfizer'
+                                AND MaxStorageTemperature > -70
+                                '''
+
+                    self.vaccine_a.AddDoses(1, cursor)
+
+                    cursor.execute(sqlQuery)
+                    rows = cursor.fetchall()
+
+                    if rows != []:
+                        self.fail("Pfizer Temperature problem")
+
+                    clear_tables(sqlClient)
+
+                except Exception:
+                    # clear the tables if an exception occurred
+                    clear_tables(sqlClient)
+                    self.fail("Pfizer Temperature problem")
+
+    def testTemp_doseJJ(self):
+
+        with SqlConnectionManager(Server=os.getenv("Server"),
+                                  DBname=os.getenv("DBName"),
+                                  UserId=os.getenv("UserID"),
+                                  Password=os.getenv("Password")) as sqlClient:
+            with sqlClient.cursor(as_dict=True) as cursor:
+                try:
+                    # clear the tables before testing
+                    clear_tables(sqlClient)
+                    # create a new VaccineCaregiver object
+                    self.vaccine_a = covid(vaccine="J&J",
+                                                    cursor=cursor)
+                    # check if the patient is correctly inserted into the database
+                    sqlQuery = '''
+                                SELECT MaxStorageTemperature
+                                FROM Vaccine
+                                WHERE VaccineName = 'J&J'
+                                AND MaxStorageTemperature > 90
+                                '''
+
+                    self.vaccine_a.AddDoses(1, cursor)
+
+                    cursor.execute(sqlQuery)
+                    rows = cursor.fetchall()
+
+                    if rows != []:
+                        self.fail("J&J Temperature problem")
+
+                    clear_tables(sqlClient)
+
+                except Exception:
+                    # clear the tables if an exception occurred
+                    clear_tables(sqlClient)
+                    self.fail("J&J Temperature problem")
 
 if __name__ == '__main__':
     unittest.main()
-
-                        
